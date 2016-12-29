23c53e966a00885505ed446c878651e327683090
Joanna <asiad706@gmail.com>
2016-12-29 22:27:56 +0100

    mobile menu and fixes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     window.sr = ScrollReveal().reveal('.scroll');
+
+    /*$("#moblie-menu-close").click(function () {
+        $(this).closest("ul").removeClass("moblie-shown");
+    });
+
+    $("#moblie-menu-open").click(function () {
+        $(this).closest("ul").addClass("moblie-shown");
+    });*/
+
+    $("#moblie-menu-open, #moblie-menu-close").click(function () {
+        $(this).closest("ul").toggleClass("moblie-shown");
+    });
 });
