4beb4d2362b4b5daf0b5eed87303915052392358
Tomasz Chmiel <chmieltomek94@gmail.com>
2016-12-30 14:57:55 +0100

    no animation

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
 $(document).ready(function () {
     window.sr = ScrollReveal().reveal('.scroll');
-    $(window).resize(function () {
-        if($(this).width() > 1024) {
-            $(".hamburger").hide();
-            $(".header-nav-list").show();
-        }
-    });
-    $(".cross").hide();
-    $(".mobile-shown").hide();
-    $(".hamburger").click(function () {
-        $(".mobile-shown").slideToggle("slow", function () {
-            $(".hamburger").hide();
-            $(".cross").show();
-        });
+    /*$("#moblie-menu-close").click(function () {
+        $(this).closest("ul").removeClass("moblie-shown");
     });
 
-    $(".cross").click(function () {
-        $(".mobile-shown").slideToggle("slow", function () {
-            $(".cross").hide();
-            $(".hamburger").show();
-        });
+    $("#moblie-menu-open").click(function () {
+        $(this).closest("ul").addClass("moblie-shown");
+    });*/
+
+    $("#moblie-menu-open, #moblie-menu-close").click(function () {
+        $(this).closest("ul").toggleClass("moblie-shown");
     });
 });
