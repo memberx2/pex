88e7fd7662c64970cf2ebc16f7db4804c593e89a
Joanna <asiad706@gmail.com>
2017-01-03 00:33:37 +0100

    main.js fixes, main remove

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
-  window.sr = ScrollReveal().reveal('.scroll')
-  var $menu = $('.header-nav-list')
-  $('.hamburger-menu-spans').click(function (e) {
-    e.preventDefault()
-    $menu.slideToggle(function () {
-      $('.span1').toggleClass('s1-open')
-      $('.span2').toggleClass('s2-open')
-      $('.span3').toggleClass('s3-open')
+    window.sr = ScrollReveal().reveal('.scroll')
+    var $menu = $('.header-nav-list')
+
+    $(window).resize(function () {
+        if (window.innerWidth > 1024) {
+            menu.show();
+        }
+    });
+
+    $('.hamburger-menu-spans').click(function (e) {
+        e.preventDefault()
+        $menu.slideToggle(function () {
+            $('.span1').toggleClass('s1-open')
+            $('.span2').toggleClass('s2-open')
+            $('.span3').toggleClass('s3-open')
+        })
     })
-  })
 })
