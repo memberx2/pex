d7bed551983176a5e8e3251f054c0fe628c23099
Krzysztof Lojniewski <kontakt@pagepro.pl>
2017-01-02 14:53:41 +0100

    object-fit, overflow fi

--- js/main.js
+++ js/main.js
@@ -1,35 +1,12 @@
 $(document).ready(function () {
-    window.sr = ScrollReveal().reveal('.scroll');
-
-   /* var closeButton = $("#close-menu-button");
-    var openButton = $("#open-menu-button");
-    var menu = $(".header-nav-list");
-
-    closeButton.click(function () {
-        menu.slideToggle(function () {
-            closeButton.hide();
-            openButton.show();
-        });
-    });
-
-    openButton.click(function () {
-        closeButton.show();
-        menu.slideToggle(function () {
-            openButton.hide();
-        });
-    });
-*/
-    $(window).resize(function(){
-        if(window.innerWidth > 1024) {
-            menu.show();
-        } 
-    });
-var menu = $(".header-nav-list");
-	$('.hamburger-menu-spans').click(function(){
-        menu.slideToggle(function () {
-		$('.span1').toggleClass('s1-open');
-        $('.span2').toggleClass('s2-open');
-        $('.span3').toggleClass('s3-open');
-	});
-    });
-});
+  window.sr = ScrollReveal().reveal('.scroll')
+  var $menu = $('.header-nav-list')
+  $('.hamburger-menu-spans').click(function (e) {
+    e.preventDefault()
+    $menu.slideToggle(function () {
+      $('.span1').toggleClass('s1-open')
+      $('.span2').toggleClass('s2-open')
+      $('.span3').toggleClass('s3-open')
+    })
+  })
+})
